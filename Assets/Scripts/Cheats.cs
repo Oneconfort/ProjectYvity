f8e076c7e6cfb0976a414d9580e5aa0f9131b4fe
Arnarck <andreluismolcalais@gmail.com>
2024-11-04 13:24:33 -0300

    Fixed 'null' campfires when using the 'teleport' cheat.

--- Assets/Scripts/Cheats.cs
+++ Assets/Scripts/Cheats.cs
@@ -5,54 +5,26 @@ using UnityEngine;
 public static class Cheats
 {
     public static int campFireIndex;
-    public static CampFire[] campFires;
 
     public static void GoToNextCampFire()
     {
         campFireIndex++;
-        if (!MaybeLoadCampFires())
-        {
-            return;
-        }
-        
-        if (campFireIndex >= campFires.Length)
+        if (campFireIndex >= GameController.controller.campFires.Length)
         {
             campFireIndex = 0;
         }
 
-        GameController.controller.Player.Respawn(campFires[campFireIndex]);
+        GameController.controller.Player.Respawn(GameController.controller.campFires[campFireIndex]);
     }
 
     public static void GoToPreviousCampFire()
     {
-        if (!MaybeLoadCampFires())
-        {
-            return;
-        }
-
         campFireIndex--;
         if (campFireIndex < 0)
         {
-            campFireIndex = campFires.Length - 1;
+            campFireIndex = GameController.controller.campFires.Length - 1;
         }
 
-        GameController.controller.Player.Respawn(campFires[campFireIndex]);
-    }
-
-    static bool MaybeLoadCampFires()
-    {
-        if (campFires == null || campFires.Length == 0)
-        {
-            campFires = Object.FindObjectsOfType<CampFire>();
-            if (campFires == null || campFires.Length == 0)
-            {
-                Debug.Assert(false, "There is no camp fires in this level.");
-                return false;
-            }
-
-            campFireIndex = 0;
-        }
-
-        return true;
+        GameController.controller.Player.Respawn(GameController.controller.campFires[campFireIndex]);
     }
 }
